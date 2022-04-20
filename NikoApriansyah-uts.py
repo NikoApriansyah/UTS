8971fd9c186e264bf980a3908e191d0bdeddc6d9
NikoApriansyah <104054593+NikoApriansyah@users.noreply.github.com>
2022-04-20 13:10:02 +0700

    Add files via upload

---
+++ NikoApriansyah-uts.py
@@ -0,0 +1,19 @@
+print ('='*50)
+print ('Nama    : Niko Apriansyah')
+print ('NIM     : 191011400383')
+print ('Kelas   : 06TPLM003')
+print ('Mapel   : Kecerdasan Buatan')
+
+print ('='*50)
+print ('Nim ganjil : tutorial looping')
+print ('='*50)
+
+# Program untuk iterasi list menggunakan pengindeksan
+matkul = ['Kecerdasan Buatan', 'Pemrograman Web', 'Matematika', 'Mobile Programing', 'Bahasa Inggris']
+
+# iterasi list menggunakan indeks
+for i in range(len(matkul)):
+    print("Saya Suka Mata Kuliah", matkul[i])
+    
+    
+print ('='*50)
